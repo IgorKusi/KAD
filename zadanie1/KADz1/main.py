4d6a5f923b637b1d40ad5d8dac109ea0c01408d5
242442 Igor Kusideł <242442@edu.p.lodz.pl>
2022-10-24 18:47:38 +0200

    KADzad1 NewProject

--- zadanie1/KADz1/main.py
+++ zadanie1/KADz1/main.py
@@ -1,15 +1,14 @@
 import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
 import Func as f
 
 # wczytywanie danych z pliku
 dane = pd.read_csv('kad.csv', header=None, names=["DP", "SP", "DK", "SK", "ID"])
-Dlug_plat = pd.read_csv(r'kad.csv',header=None, usecols=[0])
+Dlug_plat = pd.read_csv(r'kad.csv', header=None, usecols=[0])
 Szer_plat = pd.read_csv(r'kad.csv', header=None, usecols=[1])
 Dlug_kiel = pd.read_csv(r'kad.csv', header=None, usecols=[2])
 Szer_kiel = pd.read_csv(r'kad.csv', header=None, usecols=[3])
-ID_Gat = pd.read_csv(r'kad.csv',header=None, usecols=[4])
+ID_Gat = pd.read_csv(r'kad.csv', header=None, usecols=[4])
 
 # tablica z wartosciami długosci płatków
 tabela_dlugosci_platkow = Dlug_plat.squeeze()
