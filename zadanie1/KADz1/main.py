aced3f1965bd883ebf3b2411c2e05984932e428b
242442 Igor Kusideł <242442@edu.p.lodz.pl>
2022-10-31 17:56:29 +0100

    Dodano moliwnosc wprowadzenia nazwy pliku z danymi

--- zadanie1/KADz1/main.py
+++ zadanie1/KADz1/main.py
@@ -3,14 +3,15 @@ import pandas as pd
 import Tabele as T
 import Wykresy as W
 
+plik=input("Proszę podać nazwę pliku lub ścieżkę: ")
 # wczytywanie danych z pliku
-dane = pd.read_csv('Dane_Irysy.csv', header=None, names=["Długość działki kielicha", "Szerokość działki kielicha",
+dane = pd.read_csv(plik, header=None, names=["Długość działki kielicha", "Szerokość działki kielicha",
                                                   "Długość płatka", "Szerokość płatka", "ID"])
-Dlug_dzial_kiel = pd.read_csv(r'Dane_Irysy.csv', header=None, usecols=[0])  # kolumna z wartościami długości działki kielicha
-Szer_dzial_kiel = pd.read_csv(r'Dane_Irysy.csv', header=None, usecols=[1])  # kolumna z wartościami szerokości działki kielicha
-Dlug_plat = pd.read_csv(r'Dane_Irysy.csv', header=None, usecols=[2])        # kolumna z wartościami długości płatka
-Szer_plat = pd.read_csv(r'Dane_Irysy.csv', header=None, usecols=[3])        # kolumna z wartościami szerokości płatka
-ID_Gat = pd.read_csv(r'Dane_Irysy.csv', header=None, usecols=[4])           # kolumna z ID poszczególnych gatunków
+Dlug_dzial_kiel = pd.read_csv(plik, header=None, usecols=[0])  # kolumna z wartościami długości działki kielicha
+Szer_dzial_kiel = pd.read_csv(plik, header=None, usecols=[1])  # kolumna z wartościami szerokości działki kielicha
+Dlug_plat = pd.read_csv(plik, header=None, usecols=[2])        # kolumna z wartościami długości płatka
+Szer_plat = pd.read_csv(plik, header=None, usecols=[3])        # kolumna z wartościami szerokości płatka
+ID_Gat = pd.read_csv(plik, header=None, usecols=[4])           # kolumna z ID poszczególnych gatunków
 
 
 tabela_dlugosci_dzialki_kielicha = Dlug_dzial_kiel.squeeze() # funkcja squeeze zamienia wczytane kolumny danych na zwykłe tablice jednowymiarowe
