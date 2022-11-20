ccddf406114880aac27a4787d317a76a1519614f
242442 Igor Kusideł <242442@edu.p.lodz.pl>
2022-11-20 17:31:38 +0100

    added TODO in main

--- zadanie2/main.jl
+++ zadanie2/main.jl
@@ -3,10 +3,13 @@ using DataFrames
 
 include("functions.jl")
 
+#TODO wyciagnac z DataFrame pojedyncze kolumny jako Array, ew wczytac je od razu do arraya 
+#TODO osobny plik na wykresy, ustalic z jakiej biblioteki bedizemy korzstac do tego 
+#TODO sprawdzic czy funckje statystyczne dzialaja jak powinny - porownac z tym co na stronie przedmiotu, ewentualnie zrobic w excelu 
+#TODO caly aspekt regline itd
 
 col_names = ["DDK", "SDK", "DP", "SP", "Gatunek"]
 df = CSV.read("dane.csv", header = col_names, DataFrame)    #loading data from .csv file to DataFrame structure 
-
 
 
 print(df);
