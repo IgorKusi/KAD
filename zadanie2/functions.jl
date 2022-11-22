f9cd87e38544413ffa68f2a072075054c912bf20
242442 Igor Kusideł <242442@edu.p.lodz.pl>
2022-11-22 12:03:01 +0100

    minor tweaks with vectors

--- zadanie2/functions.jl
+++ zadanie2/functions.jl
@@ -1,5 +1,5 @@
 #minimum tablicy 
-function mini(arr::Vector{Float64})
+function mini(arr::Vector)
     var = arr[1] #Julia indeksuje tablice od 1 nie od 0
     for i in range(1, lastindex(arr))
         if arr[i]< var
@@ -10,7 +10,7 @@ function mini(arr::Vector{Float64})
 end
 
 #maksimum tablicy 
-function maxi(arr::Vector{Float64})
+function maxi(arr::Vector)
     var = arr[1] #Julia indeksuje tablice od 1 nie od 0
     for i in range(1,lastindex(arr))
         if arr[i]> var
@@ -21,7 +21,7 @@ function maxi(arr::Vector{Float64})
 end
 
 #średnia arytmetyczna 
-function avg(arr::Vector{Float64}) 
+function avg(arr::Vector) 
     summ = 0
     for i in range(1,lastindex(arr))
         summ = summ + arr[i]
@@ -30,8 +30,8 @@ function avg(arr::Vector{Float64})
 end
 
 #kowariancja pomiedzy X a Y 
-function cov(arrX::Vector{Float64}, arrY::Vector{Float64})
-    pr::Vector{Float64} = []
+function cov(arrX::Vector, arrY::Vector)
+    pr::Vector = []
     for i in range(1, lastindex(arrX))
         push!(pr, arrX[i] * arrY[i])
     end
@@ -45,7 +45,7 @@ function cov(arrX::Vector{Float64}, arrY::Vector{Float64})
 end
 
 #odchylenie standardowe z populacji
-function sd(arr::Vector{Float64}) 
+function sd(arr::Vector) 
     av = avg(arr)
     summ = 0
     for i in range(1,lastindex(arr))
@@ -54,18 +54,18 @@ function sd(arr::Vector{Float64})
     return sqrt(summ/lastindex(arr))
 end
 
-function Pearson(arrX::Vector{Float64}, arrY::Vector{Float64})
+function Pearson(arrX::Vector, arrY::Vector)
    return (cov(arrX, arrY))/(sd(arrX)*sd(arrY))
 end
 
 #funkcja zwracajaca wspolczynnik kierunkowy a regresji liniowej 
-function regLine_A(arrX::Vector{Float64}, arrY::Vector{Float64})
+function regLine_A(arrX::Vector, arrY::Vector)
     #https://www.statystyczny.pl/regresja-liniowa/
 
     #krok 1 - obliczam roznice miedzy x, a avg(x); tak samo dla y  
    
-    arrDeltaX::Vector{Float64} = []     #tablica różnic pomiedzy kolejnymi X a średnia arytmetyczną ze wszystkich X
-    arrDeltaY::Vector{Float64} = []     # ^ , ale Y  
+    arrDeltaX::Vector = []     #tablica różnic pomiedzy kolejnymi X a średnia arytmetyczną ze wszystkich X
+    arrDeltaY::Vector = []     # ^ , ale Y  
     avgX = avg(arrX)                     #zapisuje te avg w zmiennych zeby nie wywoływać 300 razy funkcji w pętli ponizej
     avgY = avg(arrY)
     for i in range(1,lastindex(arrX))
@@ -75,14 +75,14 @@ function regLine_A(arrX::Vector{Float64}, arrY::Vector{Float64})
 
     #krok 2 - mnoze te różnice 
 
-    arrDeltasMultiplied::Vector{Float64}  = []
+    arrDeltasMultiplied::Vector  = []
     for i in range(1,lastindex(arrX))
         push!(arrDeltasMultiplied, *(arrDeltaX[i], arrDeltaY[i]))
     end
 
     #krok 3 - tablica roznic x i avg(x) podniesiona do kwadratu 
 
-    arrDeltaXsquare::Vector{Float64} = []
+    arrDeltaXsquare::Vector = []
     for i in range(1,lastindex(arrX))
         push!(arrDeltaXsquare, *(arrDeltaX[i], arrDeltaX[i]) )
     end
@@ -100,10 +100,10 @@ function regLine_A(arrX::Vector{Float64}, arrY::Vector{Float64})
     #krok 5 - obliczam a 
     a = sumArrDeltasMultiplied / sumArrDeltaXsquare
     return a;
-
+    
 end
 
-function regLine_B(arrX::Vector{Float64}, arrY::Vector{Float64})
+function regLine_B(arrX::Vector, arrY::Vector)
     avgX = avg(arrX)
     avgY = avg(arrY)
     a = regLine_A(arrX, arrY)
@@ -112,7 +112,7 @@ function regLine_B(arrX::Vector{Float64}, arrY::Vector{Float64})
     return b
 end
 
-function regLine_Wzor(arrX::Vector{Float64}, arrY::Vector{Float64})
+function regLine_Wzor(arrX::Vector, arrY::Vector)
     #poki co zostawiam to tak jak jest - w 3 osobnych funkcjach - bo nie wiem jak bedziemy korzystac z tych wartosci 
     #i nie znam sie na Julce (kiedys z jedna krecilem i mnie olała)
     #(jestem zemstą)
