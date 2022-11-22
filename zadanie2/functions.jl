c485f15247b9f0338588039ec392fe20288b0429
242442 Igor Kusideł <242442@edu.p.lodz.pl>
2022-11-22 01:28:59 +0100

    added reading from file to Vector
    fixed functions that didn't return any value and didn't want to take Vector as an argument, now they return value. But that value is incorrect.

--- zadanie2/functions.jl
+++ zadanie2/functions.jl
@@ -1,7 +1,7 @@
 #minimum tablicy 
-function mini(arr::Array{Number})
+function mini(arr::Vector{Float64})
     var = arr[1] #Julia indeksuje tablice od 1 nie od 0
-    for i in arr
+    for i in range(1, lastindex(arr))
         if arr[i]< var
             var = arr[i] 
         end
@@ -10,9 +10,9 @@ function mini(arr::Array{Number})
 end
 
 #maksimum tablicy 
-function maxi(arr::Array{Number})
+function maxi(arr::Vector{Float64})
     var = arr[1] #Julia indeksuje tablice od 1 nie od 0
-    for i in arr
+    for i in range(1,lastindex(arr))
         if arr[i]> var
             var = arr[i] 
         end
@@ -21,66 +21,69 @@ function maxi(arr::Array{Number})
 end
 
 #średnia arytmetyczna 
-function avg(arr::Array{Number}) 
+function avg(arr::Vector{Float64}) 
     summ = 0
-    for i in arr
+    for i in range(1,lastindex(arr))
         summ = summ + i
-    summ/length(arr);
     end
+    return summ/lastindex(arr)
 end
 
 #kowariancja pomiedzy X a Y 
-function cov(arrX::Array{Number}, arrY::Array{Number})
-    pr::Array{Number} =[0]
-    Ex = avg(arrX)
-    Ey = avg(arrY)
-    for i in range(length(arrX))
-        pr[i] = arrX[i] * arrY[i]
-    
-    avg(pr) - (Ex * Ey);
+function cov(arrX::Vector{Float64}, arrY::Vector{Float64})
+    pr::Vector{Float64} = []
+    for i in range(1, lastindex(arrX))
+        push!(pr, arrX[i] * arrY[i])
     end
+    avgX = avg(arrX)
+    avgY = avg(arrY)
+    avgPr = avg(pr)
+
+    avgXY = avgX * avgY 
+    return avgPr - avgXY
+
 end
 
 #odchylenie standardowe z populacji
-function sd(arr::Array{Number}) 
+function sd(arr::Vector{Float64}) 
     av = avg(arr)
     summ = 0
-    for i in arr
+    for i in range(1,lastindex(arr))
         summ = summ + ((arr[i] - av)*(arr[i] - av))
-    sqrt(summ/length(arr));
     end
+    return sqrt(summ/lastindex(arr))
 end
 
-function Pearson(arrX::Array{Number}, arrY::Array{Number})
-    (cov(arrX, arrY))/(sd(arrX)*sd(arrY))
+function Pearson(arrX::Vector{Float64}, arrY::Vector{Float64})
+   return (cov(arrX, arrY))/(sd(arrX)*sd(arrY))
 end
 
-function regLine(arrX::Array{Number}, arrY::Array{Number})
+function regLine(arrX::Vector{Float64}, arrY::Vector{Float64})
     #https://www.statystyczny.pl/regresja-liniowa/
 
     #krok 1 - obliczam roznice miedzy x, a avg(x); tak samo dla y  
    
-    arrDeltaX::Array{Number} = [0]     #tablica różnic pomiedzy kolejnymi X a średnia arytmetyczną ze wszystkich X
-    arrDeltaY::Array{Number} = [0]     # ^ , ale Y  
+    arrDeltaX::Vector{Float64} = []     #tablica różnic pomiedzy kolejnymi X a średnia arytmetyczną ze wszystkich X
+    arrDeltaY::Vector{Float64} = []     # ^ , ale Y  
     avgX = avg(arrX)                     #zapisuje te avg w zmiennych zeby nie wywoływać 300 razy funkcji w pętli ponizej
     avgY = avg(arrY)
-    for i in lastindex(arrX)
-        arrDeltaX[i] = arrX[i] - avgX 
-        arrDeltaY[i] = arrY[i] - avgY
+    for i in range(1,lastindex(arrX))
+        push!(arrDeltaX, arrX[i] - avgX) 
+        push!(arrDeltaY, arrY[i] - avgY)
     end
 
     #krok 2 - mnoze te różnice 
 
-    arrDeltasMultiplied::Array{Number}  = [0]
-    for i in lastindex(arrX)
-        arrDeltasMultiplied[i] = *(arrDeltaX[i], arrDeltaY[i]) #ale fancy mnozenie hehe lols 
+    arrDeltasMultiplied::Vector{Float64}  = []
+    for i in range(1,lastindex(arrX))
+        push!(arrDeltasMultiplied, *(arrDeltaX[i], arrDeltaY[i]))
     end
 
     #krok 3 - tablica roznic x i avg(x) podniesiona do kwadratu 
 
-    arrDeltaXsquare::Array{Number} = [0]
-    for i in lastindex(arrX)
-        arrDeltaXsquare[i] = *(arrDeltaX[i], arrDeltaX[i])
+    arrDeltaXsquare::Vector{Float64} = []
+    for i in range(1,lastindex(arrX))
+        push!(arrDeltaXsquare, *(arrDeltaX[i], arrDeltaX[i]) )
     end
 
     #krok 4 -sumuje wszystkie wartosci w tablicy z kroku 2, i wszystkie wartosci z kroku 3 (nie ze sobą, tylko osobno dwie sumy poszczegolnych tablic)
@@ -88,7 +91,7 @@ function regLine(arrX::Array{Number}, arrY::Array{Number})
     sumArrDeltasMultiplied = 0
     sumArrDeltaXsquare = 0
 
-    for i in lastindex(arrX)
+    for i in range(1,lastindex(arrX))
         sumArrDeltasMultiplied = sumArrDeltasMultiplied + arrDeltasMultiplied[i]
         sumArrDeltaXsquare = sumArrDeltaXsquare + arrDeltaXsquare[i]
     end
--- zadanie2/main.jl
+++ zadanie2/main.jl
@@ -3,12 +3,34 @@ using DataFrames
 
 include("functions.jl")
 
-#TODO wyciagnac z DataFrame pojedyncze kolumny jako Array, ew wczytac je od razu do arraya 
 #TODO osobny plik na wykresy, ustalic z jakiej biblioteki bedizemy korzstac do tego 
 #TODO sprawdzic czy funckje statystyczne dzialaja jak powinny - porownac z tym co na stronie przedmiotu, ewentualnie zrobic w excelu 
 
-col_names = ["DDK", "SDK", "DP", "SP", "Gatunek"]
-df = CSV.read("dane.csv", header = col_names, DataFrame)    #loading data from .csv file to DataFrame structure 
+
+#_______________________________________________________WCZYTYWANIE Z PLIKU_______________________________________________________#
+
+col_names = ["DDK", "SDK", "DP", "SP", "Gatunek"]                                       #array stringow z labelami kolumn -> pewnie niepotrzebne, ale bardziej czytelne wczytywanie kolumn w dalszych krokach
+
+df = CSV.read("dane.csv", header = col_names, DataFrame)                                #zasysam dane z csv do DataFrame, wszystko jak leci 
 
 
-print(df);
+#archaizm - nie jest to już wcale potrzebne, zostawiłem zeby było widac ze w razie co to sie da
+# |
+# V
+#dfDDK = CSV.read("dane.csv", header = col_names, select = [:DDK], DataFrame)            #zasysam tylko kolumne z labelem "DDK"
+#dfSDK = CSV.read("dane.csv", header = col_names, select = [:SDK], DataFrame)            
+#dfDP = CSV.read("dane.csv", header = col_names, select = [:DP], DataFrame)
+#dfSP = CSV.read("dane.csv", header = col_names, select = [:SP], DataFrame)
+#dfGatunek = CSV.read("dane.csv", header = col_names, select = [:Gatunek], DataFrame)
+
+DDK = df.DDK                                                            #konwertuje kolumne DataFrame na vector floatow (yes, indeed it is that simple)
+SDK = df.SDK                                                            #vector to własciwie alias dla typu Array{T, 1} gdzie T to typ zmiennych
+DP = df.DP                                                              #w ten sam spobób jak Matrix to Array{T, 2}
+SP = df.SP
+Gatunek = df.Gatunek
+
+
+#print(typeof(DDK),"\n", DDK, "\n","\n",typeof(SDK),"\n",SDK,"\n","\n",typeof(DP),"\n",DP,"\n","\n",typeof(SP),"\n",SP,"\n","\n",typeof(Gatunek),"\n",Gatunek )
+
+print(Pearson(SDK, DDK))
+print("\n", regLine(DDK, SDK))
